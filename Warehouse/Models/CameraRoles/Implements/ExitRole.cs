4211e629dd2cb213525f9fffc35c4c20c978d2ed
DezmontDeXa <flatron2800@gmail.com>
2023-04-25 17:09:45 +0300

    Update ExitRole.cs

--- Warehouse/Models/CameraRoles/Implements/ExitRole.cs
+++ Warehouse/Models/CameraRoles/Implements/ExitRole.cs
@@ -9,12 +9,54 @@ namespace Warehouse.Models.CameraRoles.Implements
         public ExitRole(ILogger logger, WaitingListsService waitingList) : base(logger, waitingList)
         {
             Name = "Выезд";
-            Description = "Машина выезжает с территории";
+            Description = "Обнаружение машин на выезд и открытие шлагбаума";
         }
 
         protected override void OnExecute(Camera camera, CameraNotifyBlock notifyBlock, CarAccessInfo carAccessInfo, string plateNumber, string direction)
         {
-            throw new NotImplementedException();
+            if (carAccessInfo.Car == null || carAccessInfo.List == null)
+            {
+                ProcessNotTrakedCar(camera, plateNumber, carAccessInfo);
+                return;
+            }
+
+            switch (carAccessInfo.AccessType)
+            {
+                case AccessGrantType.Free:
+                    ProcessFreeCar(camera, notifyBlock, carAccessInfo, plateNumber, direction);
+                    break;
+                case AccessGrantType.Tracked:
+                    ProcessTrackedCar(camera, notifyBlock, carAccessInfo, plateNumber, direction);
+                    break;
+            }
+        }
+
+        private void ProcessNotTrakedCar(Camera camera, string plateNumber, CarAccessInfo carAccessInfo)
+        {
+            //TODO: Отправить распознаный номер в специальную таблицу БД для дальнейшей обработки охранником.
+            Logger.Warn($"{camera.Name}: Обнаружена неезарегистрированная машина ({plateNumber}) или ее нет в списках. Уведомляем кпп.");
+        }
+
+        private void ProcessFreeCar(Camera camera, CameraNotifyBlock notifyBlock, CarAccessInfo carAccessInfo, string plateNumber, string direction)
+        {
+            Logger.Info($"{camera.Name}: Выезжает машина из списка {carAccessInfo.List.Name} с номером ({plateNumber}). Открыть шлагбаум. Сменить статус на \"Ожидается\"");
+            OpenBarrier(camera, carAccessInfo);
+        }
+
+        private void ProcessTrackedCar(Camera camera, CameraNotifyBlock notifyBlock, CarAccessInfo carAccessInfo, string plateNumber, string direction)
+        {
+            if (carAccessInfo.Car.State == null)
+            {
+                Logger.Warn($"{camera.Name}: Машина {plateNumber} не имеет статуса. Без действий.");
+                return;
+            }
+
+        }
+
+
+        private void OpenBarrier(Camera camera, CarAccessInfo carAccessInfo)
+        {
+            //TODO: Открыть шлагбаум
         }
     }
 }
