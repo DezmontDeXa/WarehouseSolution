adaaf6e71c82e25deeaaf5a6416a195e6f520fa6
DezmontDeXa <flatron2800@gmail.com>
2023-04-25 16:53:29 +0300

    Enable TimeService

--- Warehouse/Program.cs
+++ Warehouse/Program.cs
@@ -1,9 +1,11 @@
 ﻿using Ninject;
 using Warehouse;
 using Warehouse.Models.CameraRoles;
+using Warehouse.Services;
 
 var _kernel = new StandardKernel(new Container());
 _kernel.Get<CameraRolesToDB>().AddExistingCameraRolesToDB();
+_kernel.Get<TimeControlService>();
 _kernel.Get<WarehouseSystem>().Run();
 
 Console.WriteLine("System started");
