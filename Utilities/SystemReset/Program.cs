b6c9c7a32ad39a51973d68d0a4183f8ac7e6545a
DezmontDeXa <flatron2800@gmail.com>
2023-06-02 11:39:28 +0300

    SystemReset extend date to two last days

--- Utilities/SystemReset/Program.cs
+++ Utilities/SystemReset/Program.cs
@@ -1,4 +1,6 @@
-﻿using SharedLibrary.DataBaseModels;
+﻿using Microsoft.EntityFrameworkCore;
+using SharedLibrary.DataBaseModels;
+using Warehouse.Models.CarStates.Implements;
 
 using (var db = new WarehouseContext())
 {
@@ -7,6 +9,19 @@ using (var db = new WarehouseContext())
 
     foreach (var timer in db.CarStateTimers)
         timer.IsAlive = false;
+
+    foreach (var car in db.Cars)
+        if (car.CarStateId != new AwaitingState().Id)
+            car.CarStateId = new ErrorState().Id;
+
+    var toDayLists = db.WaitingLists.Include(x=>x.Cars).Where(x => x.Date.Value.DayOfYear == DateTime.Now.DayOfYear || x.Date.Value.DayOfYear-1 == DateTime.Now.DayOfYear).ToList();
+    foreach (var list in toDayLists)
+    {
+        foreach(var car in list.Cars)
+        {
+            car.CarStateId = new AwaitingState().Id;
+        }
+    }
 
     db.SaveChanges();
 }
