2619f3eacd36a78246a308dd6270162264cdd47c
DezmontDeXa <flatron2800@gmail.com>
2023-06-02 10:30:10 +0300

    SystemReset util

---
+++ Utilities/SystemReset/Program.cs
@@ -0,0 +1,12 @@
+﻿using SharedLibrary.DataBaseModels;
+
+using (var db = new WarehouseContext())
+{
+	foreach (var car in db.Cars)
+        car.IsInspectionRequired = false;
+
+    foreach (var timer in db.CarStateTimers)
+        timer.IsAlive = false;
+
+    db.SaveChanges();
+}
