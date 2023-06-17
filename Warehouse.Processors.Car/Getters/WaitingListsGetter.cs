876571157643d4565be73c86c2c0741f8105a115
DezmontDeXa <flatron2800@gmail.com>
2023-06-17 16:09:02 +0300

    Update WaitingListsGetter.cs
    Clear expired lists

--- Warehouse.Processors.Car/Getters/WaitingListsGetter.cs
+++ Warehouse.Processors.Car/Getters/WaitingListsGetter.cs
@@ -1,5 +1,6 @@
 ﻿using NLog;
 using Warehouse.Interfaces.DataBase;
+using Warehouse.Interfaces.DataBase.Configs;
 using Warehouse.Processors.Car.Core;
 
 namespace Warehouse.Processors.Car.Getters
@@ -7,15 +8,19 @@ namespace Warehouse.Processors.Car.Getters
     public class WaitingListsGetter : CarInfoProcessorBase
     {
         private readonly IWarehouseDataBaseMethods dbMethods;
+        private readonly IWarehouseConfigDataBaseMethods configMethods;
 
-        public WaitingListsGetter(ILogger logger, IWarehouseDataBaseMethods dbMethods) : base(logger)
+        public WaitingListsGetter(ILogger logger, IWarehouseDataBaseMethods dbMethods, IWarehouseConfigDataBaseMethods configMethods) : base(logger)
         {
             this.dbMethods = dbMethods;
+            this.configMethods = configMethods;
         }
 
         protected override ProcessorResult Action(CarInfo info)
         {
-            info.WaitingLists = dbMethods.GetCarsWithWaitingLists().First(x => x.Id == info.Car.Id).WaitingLists;
+           var allLists = dbMethods.GetCarsWithWaitingLists().First(x => x.Id == info.Car.Id).WaitingLists;
+            var durationHours = configMethods.GetListAliveDuration();
+            info.WaitingLists = allLists.Where(x=>x.Date + new TimeSpan(durationHours,0,0) < DateTime.Now).ToList();
             return ProcessorResult.Next;
         }
     }
