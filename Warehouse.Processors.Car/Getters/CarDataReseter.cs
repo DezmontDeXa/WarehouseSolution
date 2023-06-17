d4100434067f61b270c6d1858fb35db239d0b8e2
DezmontDeXa <flatron2800@gmail.com>
2023-06-17 14:32:27 +0300

    Create CarDataReseter.cs

---
+++ Warehouse.Processors.Car/Getters/CarDataReseter.cs
@@ -0,0 +1,42 @@
+﻿using NLog;
+using Warehouse.CameraRoles.Implements;
+using Warehouse.CarStates.Implements;
+using Warehouse.Interfaces.DataBase;
+using Warehouse.Processors.Car.Core;
+
+namespace Warehouse.Processors.Car.Getters
+{
+    public class CarDataReseter : CarInfoProcessorBase
+    {
+        private readonly IWarehouseDataBaseMethods dbMethods;
+
+        public CarDataReseter(ILogger logger, IWarehouseDataBaseMethods dbMethods) : base(logger)
+        {
+            this.dbMethods = dbMethods;
+        }
+
+        protected override ProcessorResult Action(CarInfo info)
+        {
+            dbMethods.SetCarInspectionRequired(info.Car, false);
+            dbMethods.SetCarArea(info.Car, null);
+            dbMethods.SetCarFirstWeightning(info.Car, false);
+            dbMethods.SetCarSecondWeightning(info.Car, false);
+
+            Logger.Trace(BuildLogMessage(info, $"Машина уехала: сбрасываем территорию и веса"));
+            return ProcessorResult.Next;
+        }
+
+        protected override bool IsSuitableInfo(CarInfo info)
+        {
+            if(info.Camera.RoleId == new ExitRole().Id)
+            {
+                if (info.State == new AwaitingState())
+                    return true;
+
+                if (info.State == new FinishState())
+                    return true;
+            }
+            return false;
+        }
+    }
+}
